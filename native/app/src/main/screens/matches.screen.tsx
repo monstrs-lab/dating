622311eb6f5a3a47d22f5b17eb7abc4e14ee1e79
kuksi <plusaq@gmail.com>
2024-02-11 21:18:55 +0300

    feat: update swiper cards

--- native/app/src/main/screens/matches.screen.tsx
+++ native/app/src/main/screens/matches.screen.tsx
@@ -1,18 +1,24 @@
-import type { ReactElement } from 'react'
+import type { ReactElement }   from 'react'
 
-import type { Profile }      from '../../shared'
+import type { Profile }        from '../../shared'
 
-import { useEffect }         from 'react'
-import { useState }          from 'react'
-import React                 from 'react'
-import Swiper                from 'react-native-deck-swiper'
+import { ImageBackground }     from 'react-native'
+import { useEffect }           from 'react'
+import { useState }            from 'react'
+import { useRef }              from 'react'
+import { useWindowDimensions } from 'react-native'
+import React                   from 'react'
+import Swiper                  from 'react-native-deck-swiper'
 
-import { Box }               from '../../ui/layout'
-import { Text }              from '../../ui/text'
-import operations            from '../../operations'
+import { Box }                 from '../../ui/layout'
+import { Text }                from '../../ui/text'
+import operations              from '../../operations'
 
 export const MatchesScreen = (): ReactElement | null => {
+  const { height } = useWindowDimensions()
+  const [containerHeight, setContainerHeight] = useState<number>(0)
   const [matches, setMatches] = useState<Array<Profile>>([])
+  const ref = useRef<Swiper<Profile>>(null)
 
   useEffect(() => {
     const loadMatches = async (): Promise<void> => {
@@ -24,26 +30,49 @@ export const MatchesScreen = (): ReactElement | null => {
     loadMatches()
   }, [setMatches])
 
+  useEffect(() => {
+    if (height && containerHeight && ref?.current) {
+      ref.current.forceUpdate()
+    }
+  }, [containerHeight, height, ref])
+
   if (matches.length === 0) {
     return null
   }
 
   return (
-    <Box>
+    <Box
+      flex={1}
+      onLayout={(ev) => {
+        setContainerHeight(ev.nativeEvent.layout.height)
+      }}
+    >
       <Swiper
+        ref={ref}
+        animateCardOpacity
+        backgroundColor='transparent'
+        cardVerticalMargin={20}
+        cardHorizontalMargin={20}
+        marginBottom={height - containerHeight}
+        stackSize={2}
         cards={matches}
         renderCard={(card, index) => (
-          <Box
-            borderRadius='4'
-            flex={1}
-            justifyContent='center'
-            borderWidth={2}
-            borderColor='gray'
-            backgroundColor='white'
-          >
-            <Text>
-              {card.name} - {index}
-            </Text>
+          <Box borderRadius='24' flex={1} backgroundColor='gray' overflow='hidden'>
+            <ImageBackground
+              source={{ uri: card?.photos?.[0].url }}
+              src={card?.photos?.[0].url}
+              resizeMode='cover'
+              style={{
+                flex: 1,
+                justifyContent: 'flex-end',
+              }}
+            >
+              <Box margin='5x'>
+                <Text fontWeight='700' fontSize={28} color='white'>
+                  {card.name} - {index}
+                </Text>
+              </Box>
+            </ImageBackground>
           </Box>
         )}
       />
